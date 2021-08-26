669ecfea67d7f992eb9263bf8d9dfd80718e8877
Daniel-Avramovic <danijelgti@gmail.com>
2021-08-26 19:35:16 +0200

    finish form

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ class App extends React.Component{
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.login = this.login.bind(this);
+    this.reset = this.reset.bind(this);
   }
-  onChangeUsername(e) {
+  onChangeUsername (e){
     this.setState({ username: e.target.value })
   };
   onChangePassword(e) {
     this.setState({ password: e.target.value })
   };
   login(){
-    alert(this.username + this.password)
+    alert(this.state.username + this.state.password)
   };
   reset(){
     this.setState({
@@ -30,6 +31,10 @@ class App extends React.Component{
 render(){
   return(
   <div className="form">
+    <input type="text" value={this.state.username} onChange={this.onChangeUsername}/>
+    <input type="text" value={this.state.password} onChange={this.onChangePassword}/>
+    <button onClick={this.login}>Login</button>
+    <button onClick={this.reset}>Reset</button>
   </div>
   )
 }
